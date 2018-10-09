b33b79f2f0e3af47ff49f3be068459d1e3a284ee
Jake Dillon <jake.dillon168@gmail.com>
2018-10-09 09:15:40 -0500

    trivia questions

---
+++ Trivia Game/Trivia Questions.swift
@@ -0,0 +1,29 @@
+//
+//  Trivia Questions.swift
+//  Trivia Game
+//
+//  Created by Jake Dillon on 10/9/18.
+//  Copyright © 2018 Jake Dillon. All rights reserved.
+//
+
+import Foundation
+
+class TriviaQuestion {
+    // String storing the text of the question
+    let question: String
+    
+    // String array storing the potential answers to the question
+    let answers: [String]
+    
+    // Store the index of the correct answers in the answers array
+    let correctAnswersIndex: Int
+
+    // computed 
+    
+    
+    init (question: String, answers: [String], correctAnswerIndex: Int) {
+        self.question = question
+        self.answers = answers
+        self.correctAnswersIndex = correctAnswerIndex
+    }
+}
--- Trivia Game/ViewController.swift
+++ Trivia Game/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var triviaLabel: UILabel!
+    
+    @IBOutlet weak var resetButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   triviaLabel.text = "Welcome to the Trviva Game"
+    resetButton.setTitle("reset", for: .normal)
     }
 
-
+    @IBAction func resetButton(_ sender: Any) {
+        let newGame = true
+        if newGame == true {
+    }
+    }
 }
 
